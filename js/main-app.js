1dc3dbeb62c8065bb04c922ef61a65a6142df32a
Augusto Lucas <augustolucas92@gmail.com>
2018-01-18 20:33:44 -0200

    Melhorias de funções e implementações

--- js/main-app.js
+++ js/main-app.js
@@ -1,17 +1,48 @@
 var app = angular.module("registree", ["ngAnimate", "ui.bootstrap"]);
 
-app.controller("registreeController", function($scope){
+app.controller("registreeController", function($scope, $timeout){
 
     /**Lista de usuários */
     $scope.userList = [
         {name: "Teste",
         lastname: "Testencio",
         email: "teste@testmail.com",
-        phone: "98888-9999",
-        state: "SC",
+        phone: "(47) 98888-9999",
+        state: 24,
         city: "Joinville",
         adress: "Rua Teste, 123",
         neighborhood: "Vila Teste"}
+    ];
+
+    /**Lista de estados */
+    $scope.stateList = [
+        {stateId:  1, stateName: "AC"},
+        {stateId:  2, stateName: "AL"},
+        {stateId:  3, stateName: "AP"},
+        {stateId:  4, stateName: "AM"},
+        {stateId:  5, stateName: "BA"},
+        {stateId:  6, stateName: "CE"},
+        {stateId:  7, stateName: "DF"},
+        {stateId:  8, stateName: "ES"},
+        {stateId:  9, stateName: "GO"},
+        {stateId: 10, stateName: "MA"},
+        {stateId: 11, stateName: "MS"},
+        {stateId: 12, stateName: "MT"},
+        {stateId: 13, stateName: "MG"},
+        {stateId: 14, stateName: "PA"},
+        {stateId: 15, stateName: "PB"},
+        {stateId: 16, stateName: "PR"},
+        {stateId: 17, stateName: "PE"},
+        {stateId: 18, stateName: "PI"},
+        {stateId: 19, stateName: "RJ"},
+        {stateId: 20, stateName: "RN"},
+        {stateId: 21, stateName: "RS"},
+        {stateId: 22, stateName: "RO"},
+        {stateId: 23, stateName: "RR"},
+        {stateId: 24, stateName: "SC"},
+        {stateId: 25, stateName: "SP"},
+        {stateId: 26, stateName: "SE"},
+        {stateId: 27, stateName: "TO"}
     ];
 
     /**usuário alvo */
@@ -26,20 +57,21 @@ app.controller("registreeController", function($scope){
     $scope.selectedIndex;
     /**Verificador do checkbox para manter dialog aberta para vários registros */
     $scope.keepOpen = false;
-
+    /**Variável de controle do submit do form */
     $scope.submitted = false;
 
     /**função salvar com validação*/
     $scope.saveUser = function(isValid){
-        console.log(isValid);
         $scope.submitted = true;
         if(isValid){
             if($scope.isEdit){
                 $scope.userList[$scope.selectedIndex] = $scope.userForm;
                 $scope.message = "Usuário alterado com sucesso!";
+                $scope.clearMessage();
             }else{
                 $scope.userList.push($scope.userForm);
                 $scope.message = "Usuário criado com sucesso!";
+                $scope.clearMessage();
             }
             
             $scope.userForm = {};
@@ -48,12 +80,6 @@ app.controller("registreeController", function($scope){
             if(!$scope.keepOpen){
                 $("#userModal").modal("hide");
             }
-            
-        }else{
-            console.log("INVALIDO");
-            console.log($scope.submitted);
-            //console.log()
-            //form.classList.add('was-validated');
         }
     };
 
@@ -63,41 +89,46 @@ app.controller("registreeController", function($scope){
         $scope.title = "Editar";
 
         var userInfo = {};
-        userInfo.name = user.name;
-        userInfo.lastname = user.lastname;
-        userInfo.email = user.email;
-        userInfo.phone = user.phone;
-        userInfo.state = user.state;
-        userInfo.city = user.city;
-        userInfo.adress = user.adress;
-        userInfo.neighborhood = user.neighborhood;
+
+        let inputNames = Object.keys(user).filter(key => key.indexOf('$') !== 0);
+        for(let input of inputNames){
+            userInfo[input] = user[input];
+        }
 
         $scope.selectedIndex = $scope.userList.indexOf(user);
 
         $scope.isEdit = true;
         $scope.userForm = userInfo;
-        //$scope.userForm = user;
 
         $scope.selectedUser = user;
     };
-
     
     /**função deletar usuário */
     $scope.deleteUser = function(){
         $scope.userList.splice($scope.userList.indexOf($scope.selectedUser), 1);
         $scope.message = "Usuário excluido com sucesso!";
+        $scope.clearMessage();
     };
 
-    /**função limpar usuário alvo selecionado */
-    $scope.scopeClear = function(){
+    /**função limpar form */
+    $scope.formClear = function(form){
         $scope.userForm = {};
         $scope.isEdit = false;
         $scope.title = "Novo";
         $scope.keepOpen = false;
+        $scope.submitted = false;
+        form.$setPristine();
+        form.$setUntouched();
     };
 
     /**função limpar mensagem */
     $scope.clearMessage = function(){
-        $scope.message = "";
+        $scope.isVisible = true;
+        $timeout.cancel(time);
+
+        var time = $timeout(function() {
+            $scope.isVisible = "";
+            //$scope.message = "";
+        }, 3000);
     };
 });
